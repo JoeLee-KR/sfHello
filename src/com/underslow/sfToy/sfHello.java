08643d634ed17262d42a4aecd207cda569c306d6
Joe Lee <xjoelee@gmail.com>
2023-05-25 13:09:07 +0900

    manually at IJ

---
+++ src/com/underslow/sfToy/sfHello.java
@@ -0,0 +1,8 @@
+package com.underslow.sfToy;
+
+public class sfHello {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(":::com.underslow.sfToy.sfHello.java:::");
+    }
+}
